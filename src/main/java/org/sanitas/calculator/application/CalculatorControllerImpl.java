46774a3ff8a343ef157656aa43085593b1a579b0
fjrubio.devanddel <francisco@devanddel.com>
2023-06-15 23:03:48 +0200

    Implementación de los métodos sumar y restar de llamadas del controlador

--- src/main/java/org/sanitas/calculator/application/CalculatorControllerImpl.java
+++ src/main/java/org/sanitas/calculator/application/CalculatorControllerImpl.java
@@ -1,18 +1,25 @@
 package org.sanitas.calculator.application;
 
 import org.sanitas.calculator.domain.CalculatorController;
+import org.sanitas.calculator.domain.CalculatorService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorControllerImpl implements CalculatorController {
 
+    private final CalculatorService calculatorService;
+
+    public CalculatorControllerImpl(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
     @Override
     public Double add(double num1, double num2) {
-        return null;
+        return calculatorService.add(num1, num2);
     }
 
     @Override
     public Double subtract(double num1, double num2) {
-        return null;
+        return calculatorService.subtract(num1, num2);
     }
 }
