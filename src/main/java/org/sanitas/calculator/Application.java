425af8c09b59d269d658e76b941e20b9ac41b2c5
fjrubio.devanddel <francisco@devanddel.com>
2023-06-15 23:42:35 +0200

    Trazado de los resultados

--- src/main/java/org/sanitas/calculator/Application.java
+++ src/main/java/org/sanitas/calculator/Application.java
@@ -1,13 +1,20 @@
 package org.sanitas.calculator;
 
+import io.corp.calculator.TracerImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"org.sanitas.calculator"})
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public TracerImpl tracer() {
+        return new TracerImpl();
     }
 
 }
--- src/main/java/org/sanitas/calculator/application/Impl/CalculatorServiceImpl.java
+++ src/main/java/org/sanitas/calculator/application/Impl/CalculatorServiceImpl.java
@@ -1,17 +1,28 @@
 package org.sanitas.calculator.application.Impl;
 
+import io.corp.calculator.TracerImpl;
 import org.sanitas.calculator.domain.CalculatorService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
+
+    private TracerImpl tracer;
+
+    public CalculatorServiceImpl (TracerImpl tracer){
+        this.tracer = tracer;
+    }
     @Override
     public double add(double a, double b) {
-        return a + b;
+        double result = a + b;
+        tracer.trace(result);
+        return result;
     }
 
     @Override
     public double subtract(double a, double b) {
-        return a - b;
+        double result = a - b;
+        tracer.trace(result);
+        return result;
     }
 }
